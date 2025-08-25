d1ccb596bff6d6d50b02d8a224a2a73b8285d0be
Aljosa <aljosaspika@gmail.com>
2025-08-25 18:43:35 +0200

    Gotov cpp jebe mi se vise da ga kucam

--- 07/Zadaci/2/ucionica.hpp
+++ 07/Zadaci/2/ucionica.hpp
@@ -8,8 +8,30 @@
 class Ucionica {
     private:
     List<Osoba> spisak;
+    DinString naziv;
     int kapacitet;
     public:
+    Ucionica(const char* naziv, int kapacitet) : naziv(naziv) { this->kapacitet = kapacitet; }
+
+    bool dodajOsobu(const Osoba& o) {
+        if (spisak.size() + 1 > kapacitet)
+            return false;
+        spisak.add(spisak.size() + 1, o);
+        return true;
+    }
+
+    void ispisPrisutnih() {
+        if (spisak.size() == 0) {
+            cout << "Ucionica je prazna" << endl;
+            return;
+        }
+        Osoba o;
+        cout << "Prisutni su: " << endl;
+        for (int i = 1; i <= spisak.size(); i++) {
+            spisak.read(i, o);
+            o.ispis();
+        }
+    }
 };
 
 #endif // !UCIONICA_DEF
